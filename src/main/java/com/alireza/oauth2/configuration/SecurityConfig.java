6060b19f0f3e0ef55462b87e8b09f5567fa6f57e
phylix <alirezarastegar117390@gmail.com>
2023-09-01 22:20:39 +0330

    create SecurityConfig class for secure our endpoints

---
+++ src/main/java/com/alireza/oauth2/configuration/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.alireza.oauth2.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableMethodSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
